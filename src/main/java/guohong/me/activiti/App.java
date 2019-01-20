ea36e5cebebde10b4bf51f638a3d2478561a9504
maoguohong <guoh.mao@gmail.com>
2019-01-20 16:24:54 +0800

    fc

---
+++ src/main/java/guohong/me/activiti/App.java
@@ -0,0 +1,11 @@
+package guohong.me.activiti;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/guohong/me/activiti/AppTest.java
@@ -0,0 +1,20 @@
+package guohong.me.activiti;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
